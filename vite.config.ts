ccf0a486605057d06103c76083e044ae664b2065
Hara Keisuke <harakeisuke7@gmail.com>
2025-05-30 21:43:46 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-// NODE_ENV ではなく VERCEL_ENV でも判定可
-const isDev = process.env.NODE_ENV !== 'production'
-
 export default defineConfig({
-  base: isDev ? '/' : '/toppage/',
+  base: '/',                  // ここを常にルートに
   plugins: [vue()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
+    alias: { '@': path.resolve(__dirname, 'src') },
   },
 })
