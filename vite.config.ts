c98256ddf851b54e5282db1b4f672a131e4bbda0
Hara Keisuke <harakeisuke7@gmail.com>
2025-06-01 12:10:04 +0900

    ローカル確認２

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,22 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  base: '/',                  // ここを常にルートに
+  base: '/',                 // ルートを常に “/” に設定
   plugins: [vue()],
   resolve: {
-    alias: { '@': path.resolve(__dirname, 'src') },
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  // .html ファイルを JS として import 解析しないようにする
+  assetsInclude: ['**/*.html'],
+  server: {
+    proxy: {
+      // フロント側から /api/* へアクセスされたら、localhost:3000/api/* に自動転送する
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
   },
 })
